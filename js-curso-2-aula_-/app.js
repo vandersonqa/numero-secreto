322aeffe58b72d5d2557569381d01229e13074a7
Vanderson <vandersonqa08@gmail.com>
2024-03-14 11:35:40 -0400

    alterando limite 1 pra 100

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -11,7 +11,7 @@ function exibirTextoNaTela (tag, texto) {
 
 function exibirMensagemInicial () {
     exibirTextoNaTela('h1', 'Jogo do Número Secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirMensagemInicial();
